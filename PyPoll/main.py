78a2f8bade45f121267e2f1a78d1f9d96484f64a
Leanne <leanne_porter@hotmail.com>
2021-05-23 12:28:52 +1000

    adding vote total code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,10 @@ election_csv = os.path.join("Resources","election_data.csv")
 
 num_rows = 0
 candidate_list = []
+Khan_votes = 0
+Correy_votes = 0
+Li_votes = 0
+Otooley_votes = 0
 lines = 0
 
 with open(election_csv) as csv_file:
@@ -23,7 +27,23 @@ with open(election_csv) as csv_file:
         if row[2] not in candidate_list:
             candidate_list.append(str(row[2]))
 
+        if row[2] == "Khan":
+            Khan_votes += 1
 
+        if row[2] == "Correy":
+            Correy_votes += 1
+        
+        if row[2] == "Li":
+            Li_votes += 1
+
+        if row[2] == "O'Tooley":
+            Otooley_votes += 1
+
+ 
+print(Khan_votes)
+print(Correy_votes)
+print(Li_votes)
+print(Otooley_votes)
 print(lines)           
 print(candidate_list)
 
